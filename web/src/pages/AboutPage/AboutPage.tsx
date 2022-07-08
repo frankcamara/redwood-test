3e4d66cc80f2c28f4c4b85ddf4468bf86035c7c8
Frank Camara <frank.camara@sigmatechnology.se>
2022-07-08 19:33:54 +0200

    First commit

---
+++ web/src/pages/AboutPage/AboutPage.tsx
@@ -0,0 +1,12 @@
+import { MetaTags } from '@redwoodjs/web'
+
+const AboutPage = () => {
+  return (
+    <>
+      <MetaTags title="About" description="About page" />
+      <p>About page content</p>
+    </>
+  )
+}
+
+export default AboutPage
